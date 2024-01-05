9330d934bf57c25acfac6a0695d2bdae46ce4586
Jeremy Felt <jeremy.felt@gmail.com>
2024-01-04 22:44:02 -0800

    It's officially a plugin.

---
+++ plugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:     Comments Project
+ * Plugin URI:      https://github.com/jeremyfelt/comments-project/
+ * Description:     Comments for WordPress.
+ * Author:          jeremyfelt
+ * Author URI:      https://jeremyfelt.com
+ * Text Domain:     comments-project
+ * Domain Path:     /languages
+ * Version:         0.0.1
+ * Requires PHP:    7.4
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @package comments-project
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
